9e9bbab07c7c51e99560b4882981c610db998ff5
Jaqueline Almeida <jaqueline.almeida@tegra.com.br>
2023-08-22 19:31:12 -0300

    feat(user.repository): created user repository

--- src/infrastructure/repository/user.repository.ts
+++ src/infrastructure/repository/user.repository.ts
@@ -1,42 +1,62 @@
 import { Injectable } from '@nestjs/common';
-import { User, Prisma } from '@prisma/client';
 import { DatabaseService } from '../database/database.service';
 import { IRepository } from './repository.interface';
 import { UserDto } from 'src/application/dto/user.dto';
+import { UserModel } from '../model/user.model';
+import { UserEntity } from 'src/domain/entity/user.entity';
 
 @Injectable()
 export class UserRepository implements IRepository {
   constructor(private database: DatabaseService) {}
-  created(dto: any) {
-    throw new Error('Method not implemented.');
+
+  async created(userDto: UserDto): Promise<UserEntity> {
+    const userModel = new UserModel(userDto);
+    const newUser = await this.database.user.create({
+      data: userModel.saveUserModel(),
+    });
+
+    const userEntity = new UserEntity(newUser);
+    return userEntity;
   }
 
-  async create(userDto: UserDto): Promise<User> {
-    return this.database.user.create({
+  async findById(id: number): Promise<UserEntity | null> {
+    const user = await this.database.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) return null;
+
+    const userEntity = new UserEntity(user);
+    return userEntity;
+  }
+
+  async findAll(): Promise<UserEntity[]> {
+    const users = await this.database.user.findMany();
+
+    if (!users.length) return [];
+
+    const usersEntity = users.map((user) => new UserEntity(user));
+    return usersEntity;
+  }
+
+  async updateById(userDto: UserDto): Promise<UserEntity> {
+    const userModel = new UserModel(userDto);
+    const { data, filter } = userModel.updateUserModel();
+    const user = await this.database.user.update({
       data,
+      where: filter,
     });
+
+    const userEntity = new UserEntity(user);
+    return userEntity;
   }
 
-  async findById(id: number): Promise<User | null> {
-    return this.database.user.findUnique({
-      where: userWhereUniqueInput,
+  async deleteById(id: number): Promise<UserEntity> {
+    const user = await this.database.user.delete({
+      where: { id },
     });
-  }
 
-  async findAll(): Promise<User[]> {
-    return this.database.user.findMany();
-  }
-
-  async updateById(userDto: UserDto): Promise<User> {
-    return this.database.user.update({
-      data,
-      where,
-    });
-  }
-
-  async deleteById(id: number): Promise<User> {
-    return this.database.user.delete({
-      where,
-    });
+    const userEntity = new UserEntity(user);
+    return userEntity;
   }
 }
