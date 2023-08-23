2c4260c4803a5f056ccfffcccb55f1a5b4582792
Jaqueline Almeida <jaqueline.almeida@tegra.com.br>
2023-08-22 21:52:18 -0300

    feat(app.module): import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './application/controller/user.controller';
+import { UserService } from './domain/service/user.service';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { UserRepository } from './infrastructure/repository/user.repository';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService, UserRepository],
 })
 export class AppModule {}
